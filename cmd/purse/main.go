6ab5b61d6b8dab1abe7ed2d4dc977486e57a6617
Daved <daved@codingmusica.com>
2015-04-29 00:21:13 -0700

    Updated template comments to satisfy golint, and to correct minor typo.

--- cmd/purse/main.go
+++ cmd/purse/main.go
@@ -106,7 +106,8 @@ const (
 
 `
 
-	contentsBodyVar = `var {{.Varname}} = &GenPurse{
+	contentsBodyVar = `// {{.Varname}} is a *GenPurse.
+	var {{.Varname}} = &GenPurse{
 	files: map[string]string{
 		{{range $key, $val := .Files}}
 			"{{$key}}": {{$val}},
@@ -119,13 +120,15 @@ const (
 	"sync"
 )
 
-// GenPurse is an literal implementation of a Purse that is programmatically generated
-// from SQL file contents within a directory via go generate.
+// GenPurse is a literal implementation of a Purse that is programmatically
+// generated from SQL file contents within a directory via go generate.
 type GenPurse struct {
 	mu sync.RWMutex
 	files map[string]string
 }
 
+// Get takes a filename and returns a query if it is found within the relevant
+// map.  If filename is not found, ok will return false.
 func (p *GenPurse) Get(filename string) (v string, ok bool) {
 	p.mu.RLock()
 	v, ok = p.files[filename]
